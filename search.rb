5e9fddf5c9bf0049186e9407fcf82db18ebbf8c3
Željko Filipin <zeljko.filipin@gmail.com>
2022-02-04 15:22:46 +0100

    Explain script output

--- search.rb
+++ search.rb
@@ -11,6 +11,9 @@ duckduckgo.search(keyword)
 duckduckgo_search_results = duckduckgo.search_results
 duckduckgo.search_results_contain_keyword(duckduckgo_search_results, keyword)
 
+puts "-" * 80
+puts "DuckDuckGo search results that contain the keyword"
+puts "-" * 80
 require 'awesome_print'
 ap duckduckgo.urls_and_keywords(duckduckgo_search_results)
 
@@ -24,7 +27,14 @@ yandex.search(keyword)
 yandex_search_results = yandex.search_results
 yandex.search_results_contain_keyword(yandex_search_results, keyword)
 
+puts "-" * 80
+puts "Yandex search results that contain the keyword"
+puts "-" * 80
 ap yandex.urls_and_keywords(yandex_search_results)
+
+puts "-" * 80
+puts "Search results from both search engines"
+puts "-" * 80
 ap yandex.popular(duckduckgo_search_results, yandex_search_results)
 
 yandex.close
