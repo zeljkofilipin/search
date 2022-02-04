13ad8ea7ee38a1b7242b8071dacef15471d2240a
Željko Filipin <zeljko.filipin@gmail.com>
2022-02-04 16:41:28 +0100

    Merge duckduckgo and yandex scripts

---
+++ simple.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+browser = :chrome
+keyword = 'croatia'
+
+require './page_object/duckduckgo'
+# engine = DuckDuckGo.new(browser)
+
+require './page_object/yandex'
+engine = Yandex.new(browser)
+
+engine.search(keyword)
+
+search_results = engine.search_results
+engine.search_results_contain_keyword(search_results, keyword)
+
+require 'awesome_print'
+ap search_results
+
+engine.close
