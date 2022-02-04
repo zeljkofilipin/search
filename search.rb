a6ecd0ed7b73f1bb11640214d97bb07e5192a2df
Željko Filipin <zeljko.filipin@gmail.com>
2022-02-04 13:59:32 +0100

    Create a script to run search in two search engines

---
+++ search.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+browser = :chrome
+keyword = 'croatia'
+
+require './page_object/duckduckgo'
+duckduckgo = DuckDuckGo.new(browser)
+
+duckduckgo.search(keyword)
+
+duckduckgo_search_results = duckduckgo.search_results
+duckduckgo.search_results_contain_keyword(duckduckgo_search_results, keyword)
+
+require 'awesome_print'
+ap duckduckgo_search_results
+
+duckduckgo.close
+
+require './page_object/yandex'
+yandex = Yandex.new(browser)
+
+yandex.search(keyword)
+
+yandex_search_results = yandex.search_results
+yandex.search_results_contain_keyword(yandex_search_results, keyword)
+
+ap yandex_search_results
+
+yandex.close
