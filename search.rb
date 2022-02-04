7761474b2026fa5d91e883097bf17a6dde1307a1
Željko Filipin <zeljko.filipin@gmail.com>
2022-02-04 15:56:12 +0100

    Document the main script

--- search.rb
+++ search.rb
@@ -1,40 +1,56 @@
 # frozen_string_literal: true
 
+# configre browser and keyword
 browser = :chrome
 keyword = 'croatia'
 
+# open browser and go to the search engine
 require './page_object/duckduckgo'
 duckduckgo = DuckDuckGo.new(browser)
 
+# search for the keyword
 duckduckgo.search(keyword)
 
+# store search results in a variable
 duckduckgo_search_results = duckduckgo.search_results
+
+# check if search results contain the keyword
 duckduckgo.search_results_contain_keyword(duckduckgo_search_results, keyword)
 
+# output search results and if they contain the keyword
 puts "-" * 80
-puts "DuckDuckGo search results that contain the keyword"
+puts "DuckDuckGo search results and if they contain the keyword"
 puts "-" * 80
 require 'awesome_print'
 ap duckduckgo.urls_and_keywords(duckduckgo_search_results)
 
+# close the browser
 duckduckgo.close
 
+# open browser and go to the search engine
 require './page_object/yandex'
 yandex = Yandex.new(browser)
 
+# search for the keyword
 yandex.search(keyword)
 
+# store search results in a variable
 yandex_search_results = yandex.search_results
+
+# check if search results contain the keyword
 yandex.search_results_contain_keyword(yandex_search_results, keyword)
 
+# output search results and if they contain the keyword
 puts "-" * 80
-puts "Yandex search results that contain the keyword"
+puts "Yandex search results and if they contain the keyword"
 puts "-" * 80
 ap yandex.urls_and_keywords(yandex_search_results)
 
+# output search results that are in both search engines
 puts "-" * 80
 puts "Search results from both search engines"
 puts "-" * 80
 ap yandex.popular(duckduckgo_search_results, yandex_search_results)
 
+# close the browser
 yandex.close
